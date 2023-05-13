a9de61fbeb666cfc8d128b17744dae1781919cd6
antitank19 <trankhaiminhkhoi10a3@gmail.com>
2023-05-13 23:35:34 +0700

    Initial commit

---
+++ MeetingAppCore/MeetingAppCore/Controllers/FallbackController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeetingAppCore.Controllers
+{
+    public class FallbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine(new String('=', 10));
+            Console.WriteLine("Web/Fallback: Index()");
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        }
+    }
+}
